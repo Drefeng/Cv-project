8de15a146ab6c94c67eb68a444737f141f4335fd
Drefeng <infbil15@gmail.com>
2019-03-16 01:08:03 +0100

    data

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const data = require('../data/data.json');
 
